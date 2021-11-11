56e8ca345049c1e0fd19b32cd50c7eea47af09a0
payalmohapatra <mohapatrapayal01@gmail.com>
2021-11-11 16:37:40 -0600

    Create happiness_correlation.py
    Computes correlation matrix

---
+++ happiness_correlation.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from discover_feature_relationships import discover
+import sklearn
+
+# data input
+datapath = 'Dataset/'
+csvname = datapath + '2015.csv'
+#data = np.loadtxt(csvname,delimiter = ',')
+df_2015 = pd.read_csv("Dataset/2015.csv")
+df_2016 = pd.read_csv("Dataset/2016.csv")
+df_2017 = pd.read_csv("Dataset/2017.csv")
+
+targets = ['Low', 'Low-Mid', 'Top-Mid', 'Top']
+h_cols = ['Country', 'GDP', 'Family', 'Life', 'Freedom', 'Generosity', 'Trust']
+def prep_frame(df_year, year):
+    df = pd.DataFrame()
+    # Work around to load 2015, 2016, 2017 data into one common column
+    target_cols = []
+    for c in h_cols:
+        target_cols.extend([x for x in df_year.columns if c in x])
+    df[h_cols] = df_year[target_cols]
+    df['Happiness Score'] = df_year[[x for x in df_year.columns if 'Score' in x]]
+    # Calculate quartiles on the data.
+    df["target"] = pd.qcut(df[df.columns[-1]], len(targets), labels=targets)
+    df["target_n"] = pd.qcut(df[df.columns[-2]], len(targets), labels=range(len(targets)))
+    # Append year and assign to multi-index
+    df['Year'] = year
+    df = df.set_index(['Country', 'Year'])
+    return df
+df = prep_frame(df_2015, 2015)
+df = df.append(prep_frame(df_2016, 2016), sort=False)
+df = df.append(prep_frame(df_2017, 2017), sort=False)
+print('\n ********************Data in Tabular**************************** \n')
+print(df.head())
+
+
+spearman_cormatrix= df.corr(method='spearman')
+print('\n ********************Spearman Correlation*********************** \n')
+print(spearman_cormatrix)
